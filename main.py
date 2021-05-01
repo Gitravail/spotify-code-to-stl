262373b3507393ecada19239eaffce8987cee84e
Gitravail <raphael.tournafond@gmail.com>
2021-05-01 13:20:28 +0200

    Add size tweaking

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import stlotipy.stlotipy as stlotipy
 import matplotlib.pyplot as plt
 
+url = 'https://open.spotify.com/album/2Cv2mrKMRyYuXJTlQmY4hj?si=-bzPyrJ6QxeO-oUaGDXjyA'
+
 if __name__ == '__main__':
-    image = stlotipy.get_spotify_code_image('https://open.spotify.com/album/2Cv2mrKMRyYuXJTlQmY4hj?si=-bzPyrJ6QxeO-oUaGDXjyA')
-    plt.imshow(image)
-    plt.show()
+    sizes = stlotipy.Sizes
+    image = stlotipy.get_spotify_code_image(url, stlotipy.Sizes.MEDIUM)
+    if image:
+        plt.imshow(image)
+        plt.show()
--- stlotipy/stlotipy.py
+++ stlotipy/stlotipy.py
@@ -5,23 +5,32 @@ import re
 # Image processing
 import PIL.Image as Image
 import io
+# Misc
+from enum import Enum
 
-
-base_url = "https://scannables.scdn.co/uri/plain/png/000000/white/640/spotify:"
+base_url = "https://scannables.scdn.co/uri/plain/png/000000/white/"
 headers = {'content-type': 'image/png'}
 spotify_regex = r"https://open.spotify.com/(\w*)/([\w\d]*)\?si=[\w\d]*"
 
 
-def get_spotify_code_image(spotify_url: str):
+class Sizes:
+    SMALL = 320
+    MEDIUM = 640
+    BIG = 1080
+
+
+def get_spotify_code_image(spotify_url: str, size=640):
     """Gets and return the spotify code of a track/playlist/album from a spotify url
 
     :param spotify_url: the spotify url of the track/playlist/album
                         that you get by clicking the 3 ••• and selecting share > copy the link to ...
     :type spotify_url: str
+    :param size: width of the output image, must be a valid size (320, 480, 640, 720, 940, 1080...)
+    :type size: int
     :return: an image corresponding to the spotify code
-    :rtype: img
+    :rtype: Image
     """
-    url = _generate_scannables_url(spotify_url)
+    url = _generate_scannables_url(spotify_url, size)
     if url:
         r = requests.get(url, headers=headers)
         if r.status_code == 200:
@@ -29,18 +38,20 @@ def get_spotify_code_image(spotify_url: str):
     return None
 
 
-def _generate_scannables_url(spotify_url: str):
+def _generate_scannables_url(spotify_url: str, size=640):
     """Generate the callable scannables url to fetch the spotify code
 
     :param spotify_url: the spotify url of the track/playlist/album
                         that you get by clicking the 3 ••• and selecting share > copy the link to <resource>
     :type spotify_url: str
+    :param size: width of the output image, must be a valid size (320, 480, 640, 720, 940, 1080...)
+    :type size: int
     :return: scannables url to access the resource
     :rtype: str
     """
     m = re.match(spotify_regex, spotify_url)
     if m:
-        return base_url + m.group(1) + ':' + m.group(2)
+        return base_url + str(size) + '/spotify:' + m.group(1) + ':' + m.group(2)
     return None
 
 
